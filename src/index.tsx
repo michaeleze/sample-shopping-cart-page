e834a91cff48609481e611c16b71582e756d932f
Michael Eze <michaeleze3@gmail.com>
2021-02-10 17:52:36 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ import { ApolloProvider } from '@apollo/client';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8080/api',
-  //uri: 'https://samplecartapp.herokuapp.com/api',
+  //uri: 'http://localhost:8080/api',
+  uri: 'https://sample-shoppingcart-app.herokuapp.com/api',
   cache: new InMemoryCache()
 });
 
